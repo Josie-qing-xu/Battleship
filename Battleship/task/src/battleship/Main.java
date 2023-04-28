04c1fcf61be69460cad33ea4275483584aaad92f
josie <ms.josei.xu@gmail.com>
2023-04-28 00:01:28 -0400

    use variable replace constant

--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -6,7 +6,7 @@ public class Main {
         // Create a 10x10 field
         int BattleshipRows = 11;
         int BattleshipColumns = 11;
-        GameField BattleshipField = new GameField(11,11);
+        GameField BattleshipField = new GameField(BattleshipRows,BattleshipColumns);
         System.out.println(BattleshipField);
     }
 }
